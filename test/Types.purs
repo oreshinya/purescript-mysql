8946c346f953fca0fe585de76ba9c53979b4cc3b
Shinya Takahashi <s.takahashi313@gmail.com>
2017-07-01 14:55:57 +0900

    Add type for test

---
+++ test/Types.purs
@@ -0,0 +1,21 @@
+module Test.Types where
+
+import Prelude
+import Data.Foreign.Class (class Decode)
+import Data.Foreign.Generic (defaultOptions, genericDecode)
+import Data.Generic.Rep (class Generic)
+
+
+
+newtype User
+  = User
+    { id :: String
+    , name :: String
+    , createdAt :: String
+    , updatedAt :: String
+    }
+
+derive instance genericUser :: Generic User _
+
+instance decodeUser :: Decode User where
+  decode = genericDecode $ defaultOptions { unwrapSingleConstructors = true }
