a21b97196bf031758d438a1819c111b4256888b1
Shinya Takahashi <s.takahashi313@gmail.com>
2017-07-01 18:44:29 +0900

    Add tests

---
+++ test/Main.purs
@@ -0,0 +1,61 @@
+module Test.Main where
+
+import Prelude
+import Control.Monad.Aff (Aff, runAff)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import MySQL (MYSQL)
+import MySQL.Connection (Connection, ConnectionInfo, defaultConnectionInfo, queryWithOptions, query_, execute_)
+import MySQL.Pool (defaultPoolInfo, createPool, closePool, withPool)
+import MySQL.QueryValue (toQueryValue)
+import MySQL.Transaction (withTransaction)
+
+import Test.Types (User)
+
+
+
+ident :: String
+ident = "53f49285-a00e-46a6-b445-d25c49c228ba"
+
+ident2 :: String
+ident2 = "192fea9b-c657-4420-af76-718702bd2846"
+
+ident3 :: String
+ident3 = "649d2782-e161-4170-a1d7-cf65afdfc985"
+
+ident4 :: String
+ident4 = "45572d57-d4e5-411b-b6b6-ab6a1a8df5f9"
+
+
+
+connectionInfo :: ConnectionInfo
+connectionInfo = defaultConnectionInfo { database = "purescript_mysql", debug = true }
+
+
+
+main :: forall e. Eff (mysql :: MYSQL, exception :: EXCEPTION | e) Unit
+main = do
+  pool <- createPool connectionInfo defaultPoolInfo
+  void $ runAff (const $ closePool pool) (const $ closePool pool) do
+    flip withPool pool \conn -> do
+      execute_ "TRUNCATE TABLE users" conn
+      execute_ ("INSERT INTO users (id, name) VALUES ('" <> ident <> "', 'User 1')") conn
+      void $ selectUsers conn
+      flip withTransaction conn \c -> do
+        execute_ ("INSERT INTO users (id, name) VALUES ('" <> ident2 <> "', 'User 2')") conn
+        execute_ ("INSERT INTO users (id, name) VALUES ('" <> ident3 <> "', 'User 3')") conn
+      void $ selectUsers' conn
+      flip withTransaction conn \c -> do
+        execute_ ("INSERT INTO users (id, name) VALUES ('" <> ident4 <> "', 'User 4')") conn
+        execute_ ("INSERT INTO users (id, name) VALUES ('" <> ident <> "', 'User 5')") conn
+    where
+      opts =
+        { sql: "SELECT * FROM users WHERE id = ?"
+        , nestTables: false
+        }
+
+      selectUsers :: forall eff. Connection -> Aff (mysql :: MYSQL | eff) (Array User)
+      selectUsers = queryWithOptions opts [ toQueryValue ident ]
+
+      selectUsers' :: forall eff. Connection -> Aff (mysql :: MYSQL | eff) (Array User)
+      selectUsers' = query_ "SELECT * FROM users"
