f5b8f4df3d37484e76bcbb295884e48ca4e647e6
Shinya Takahashi <s.takahashi313@gmail.com>
2017-09-20 22:34:55 +0900

    Update test

--- test/Main.purs
+++ test/Main.purs
@@ -1,19 +1,17 @@
 module Test.Main where
 
 import Prelude
+
 import Control.Monad.Aff (Aff, runAff)
 import Control.Monad.Aff.Console as AC
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Eff.Exception (EXCEPTION)
-import Data.Array (head)
-import Data.Maybe (Maybe(..))
+import Control.Monad.Eff.Exception (EXCEPTION, message)
 import MySQL (MYSQL)
 import MySQL.Connection (Connection, ConnectionInfo, defaultConnectionInfo, queryWithOptions, query_, execute_, format)
-import MySQL.Pool (defaultPoolInfo, createPool, closePool, withPool)
+import MySQL.Pool (closePool, createPool, defaultPoolInfo, withPool)
 import MySQL.QueryValue (toQueryValue)
 import MySQL.Transaction (withTransaction)
-
 import Test.Types (User)
 
 
@@ -40,7 +38,7 @@ connectionInfo = defaultConnectionInfo { database = "purescript_mysql", debug = 
 main :: forall e. Eff (console :: CONSOLE, mysql :: MYSQL, exception :: EXCEPTION | e) Unit
 main = do
   pool <- createPool connectionInfo defaultPoolInfo
-  void $ runAff (const $ closePool pool) (success pool) do
+  void $ runAff (error pool) (success pool) do
     flip withPool pool \conn -> do
       execute_ "TRUNCATE TABLE users" conn
       execute_ ("INSERT INTO users (id, name) VALUES ('" <> ident <> "', 'User 1')") conn
@@ -49,9 +47,9 @@ main = do
         execute_ ("INSERT INTO users (id, name) VALUES ('" <> ident2 <> "', 'User 2')") conn
         execute_ ("INSERT INTO users (id, name) VALUES ('" <> ident3 <> "', 'User 3')") conn
       users <- selectUsers' conn
---      flip withTransaction conn \c -> do
---        execute_ ("INSERT INTO users (id, name) VALUES ('" <> ident4 <> "', 'User 4')") conn
---        execute_ ("INSERT INTO users (id, name) VALUES ('" <> ident <> "', 'User 5')") conn
+      --flip withTransaction conn \c -> do
+      --  execute_ ("INSERT INTO users (id, name) VALUES ('" <> ident4 <> "', 'User 4')") conn
+      --  execute_ ("INSERT INTO users (id, name) VALUES ('" <> ident <> "', 'User 5')") conn
       AC.log $ format "INSERT INTO users (id, name) VALUES (?, ?)" [ toQueryValue ident, toQueryValue "User 6"] conn
       pure users
     where
@@ -66,8 +64,11 @@ main = do
       selectUsers' :: forall eff. Connection -> Aff (mysql :: MYSQL | eff) (Array User)
       selectUsers' = query_ "SELECT * FROM users"
 
+      error pool err = do
+        log ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+        log $ message err
+        closePool pool
+
       success pool users = do
-        case (head users) of
-          Nothing -> pure unit
-          Just user -> log $ show user
+        log $ show users
         closePool pool
