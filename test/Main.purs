38724b23433d3e52f74755c5dc13fbb7497a0803
Shinya Takahashi <s.takahashi313@gmail.com>
2019-09-15 07:34:38 +0900

    Refactor

--- test/Main.purs
+++ test/Main.purs
@@ -16,7 +16,8 @@ import MySQL.QueryValue (toQueryValue)
 import MySQL.Transaction (withTransaction)
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
-import Test.Unit.Main (runTest)
+import Test.Unit.Main (run, runTestWith)
+import Test.Unit.Output.Fancy (runTest)
 
 type User =
   { id :: String
@@ -24,65 +25,59 @@ type User =
   }
 
 main :: Effect Unit
-main = runTest do
-  test "Queries" do
-    pool <- liftEffect createPool'
+main = run do
+  pool <- liftEffect createPool'
 
-    flip withPool pool \conn -> do
-      userId <- liftEffect $ genUUID <#> toString
-      let userName = "dummy_name_" <> userId
-      execute
-        "INSERT INTO users (id, name) VALUES (?, ?)"
-        [ toQueryValue userId, toQueryValue userName ]
-        conn
-      users <- query
-        "SELECT * FROM users WHERE id = ?"
-        [ toQueryValue userId ]
-        conn
-      Assert.equal
-        [ { id: userId, name: userName } ]
-        users
-
-    liftEffect $ closePool pool
-
-  suite "Transaction" do
-    test "Commit" do
-      pool <- liftEffect createPool'
+  runTestWith runTest do
+    test "Queries" do
 
       flip withPool pool \conn -> do
-        xs <- liftEffect $ replicateA 2
-          $ genUUID <#> toString <#> \id -> { id, name: "dummy_name_" <> id }
-        flip withTransaction conn $ execute
-          "INSERT INTO users (id, name) VALUES ?"
-          [ toQueryValue $ (xs <#> \x -> [ x.id, x.name ]) ]
-        users <- query
-          "SELECT * FROM users WHERE id IN (?) ORDER BY FIELD(id, ?)"
-          [ toQueryValue $ xs <#> _.id, toQueryValue $ xs <#> _.id ]
+        userId <- liftEffect $ genUUID <#> toString
+        let userName = "dummy_name_" <> userId
+        execute
+          "INSERT INTO users (id, name) VALUES (?, ?)"
+          [ toQueryValue userId, toQueryValue userName ]
           conn
-        Assert.equal xs users
+        users <- query
+          "SELECT * FROM users WHERE id = ?"
+          [ toQueryValue userId ]
+          conn
+        Assert.equal
+          [ { id: userId, name: userName } ]
+          users
 
-      liftEffect $ closePool pool
-
-    test "Rollback" do
-      pool <- liftEffect createPool'
-
-      flip withPool pool \conn -> do
-        (xs :: Array User) <- liftEffect $ replicateA 2
-          $ genUUID <#> toString <#> \id -> { id, name: "dummy_name_" <> id }
-        result <- attempt $ flip withTransaction conn \conn' -> do
-          execute
+    suite "Transaction" do
+      test "Commit" do
+        flip withPool pool \conn -> do
+          xs <- liftEffect $ replicateA 2
+            $ genUUID <#> toString <#> \id -> { id, name: "dummy_name_" <> id }
+          flip withTransaction conn $ execute
             "INSERT INTO users (id, name) VALUES ?"
             [ toQueryValue $ (xs <#> \x -> [ x.id, x.name ]) ]
-            conn'
-          throwError $ error "Rollback Test"
-        Assert.assert "Error was ignored" $ isLeft result
-        users <- query
-          "SELECT * FROM users WHERE id IN (?) ORDER BY FIELD(id, ?)"
-          [ toQueryValue $ xs <#> _.id, toQueryValue $ xs <#> _.id ]
-          conn
-        Assert.equal ([] :: Array User) users
+          users <- query
+            "SELECT * FROM users WHERE id IN (?) ORDER BY FIELD(id, ?)"
+            [ toQueryValue $ xs <#> _.id, toQueryValue $ xs <#> _.id ]
+            conn
+          Assert.equal xs users
 
-      liftEffect $ closePool pool
+      test "Rollback" do
+        flip withPool pool \conn -> do
+          (xs :: Array User) <- liftEffect $ replicateA 2
+            $ genUUID <#> toString <#> \id -> { id, name: "dummy_name_" <> id }
+          result <- attempt $ flip withTransaction conn \conn' -> do
+            execute
+              "INSERT INTO users (id, name) VALUES ?"
+              [ toQueryValue $ (xs <#> \x -> [ x.id, x.name ]) ]
+              conn'
+            throwError $ error "Rollback Test"
+          Assert.assert "Error was ignored" $ isLeft result
+          users <- query
+            "SELECT * FROM users WHERE id IN (?) ORDER BY FIELD(id, ?)"
+            [ toQueryValue $ xs <#> _.id, toQueryValue $ xs <#> _.id ]
+            conn
+          Assert.equal ([] :: Array User) users
+
+  liftEffect $ closePool pool
 
 createPool' :: Effect Pool
 createPool' = createPool connInfo defaultPoolInfo
