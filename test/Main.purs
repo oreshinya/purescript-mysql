1a7e5969104d3c4bbafd7e117e1ea36aa0817348
Shinya Takahashi <s.takahashi313@gmail.com>
2019-09-15 00:19:45 +0900

    Refactor

--- test/Main.purs
+++ test/Main.purs
@@ -3,7 +3,7 @@ module Test.Main where
 import Prelude
 
 import Control.Monad.Error.Class (throwError)
-import Data.Either (Either(..))
+import Data.Either (isLeft)
 import Data.UUID (genUUID, toString)
 import Data.Unfoldable (replicateA)
 import Effect (Effect)
@@ -14,7 +14,7 @@ import MySQL.Connection (defaultConnectionInfo, execute, query)
 import MySQL.Pool (Pool, closePool, createPool, defaultPoolInfo, withPool)
 import MySQL.QueryValue (toQueryValue)
 import MySQL.Transaction (withTransaction)
-import Test.Unit (failure, suite, test)
+import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
 
@@ -75,9 +75,7 @@ main = runTest do
             [ toQueryValue $ (xs <#> \x -> [ x.id, x.name ]) ]
             conn'
           throwError $ error "Rollback Test"
-        case result of
-          Right _ -> failure "Error was ignored"
-          _ -> pure unit
+        Assert.assert "Error was ignored" $ isLeft result
         users <- query
           "SELECT * FROM users WHERE id IN (?) ORDER BY FIELD(id, ?)"
           [ toQueryValue $ xs <#> _.id, toQueryValue $ xs <#> _.id ]
